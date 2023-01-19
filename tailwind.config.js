75a30b85c46bece14479eef9903cd8a311ca5c61
Sathvik-Malgikar <Sathvik.malgikar@gmail.com>
2023-01-19 20:24:59 +0530

    before grid

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
+      colors : {
+        foreground : "#c3f9c3",
+        background : "#87CEEB",
+      },
       fontFamily : {
         concertOne : ["Concert One"],
         Abel : ["Abel"],
