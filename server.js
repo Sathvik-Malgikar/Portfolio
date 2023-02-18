9ddf2ab83559060ceaa879e546e5a098d92136b1
Sathvik-Malgikar <Sathvik.malgikar@gmail.com>
2023-02-18 23:47:55 +0530

    projs

--- server.js
+++ server.js
@@ -8,20 +8,79 @@ let app = express()
 
 
 
-app.use(express.static("public"))
+app.use(express.static(__dirname+ "/public"))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended : true}))
+app.set("view engine" , "ejs")
+app.set("views","./views")
 
 app.post("/form/",(req,res)=>{
     mongoose.connection.db.collection("portfolio-website-messages").insertOne(req.body).then(()=>{
        console.log("uploaded data :" ,req.body )
-        res.send("uploaded succesfully !")
+        res.send("uploaded successfully !")
     }).catch(()=>{
         res.sendStatus(500)
         res.send("internal server error!")
         console.error("internal server error!")
     })
   
+
+})
+
+
+async function loadcategories(cats,alldocs){
+   let projtits = []
+    await cats.forEach( cat=>{
+        let catobjs=[]
+
+      
+        let cursor = alldocs.filter(item=>item.category==cat)
+        // console.log(cursor)
+
+        cursor.forEach( (doc,i)=>{
+            // console.log(doc,i);
+            catobjs.push( doc)
+        })
+        projtits.push({"objs"  : catobjs , "category" : cat})
+    })
+    // console.log("before retrun" , projtits);
+    return projtits
+}
+
+app.get("/projects/:name", (req,res)=>{
+
+    if( "name" in req.params && req.params.name!=''){
+        mongoose.connection.collection("portfolio-projects").findOne({"name" : req.params["name"]}).then(doc=>{
+if(!doc)
+{
+    res.send("project document not found !")
+    return
+}
+            console.log("param route doc",doc.name);
+            res.render("projpage.ejs" , {"doc" : doc})
+        })
+        
+    }else{
+        res.send("project not found!")
+    }
+
+})
+
+
+app.get("/projects/", (req,res)=>{
+    
+   
+
+    let categories=["Hackathon" , "Apps"]
+    mongoose.connection.collection("portfolio-projects").find().toArray().then(docs=>{
+        
+        loadcategories(categories,docs).then((projobjs)=>{
+    
+            console.log("before render" , projobjs);
+            res.render("projects.ejs",{ "projobjs" : projobjs})
+        })
+    })
+
 
 })
 
---
+++ views/projects.js
@@ -0,0 +1,6 @@
+let divs = document.getElementsByClassName("projdiv")
+divs.forEach(element => {
+    element.onclick = ()=>{
+        
+    }
+});
