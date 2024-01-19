ec1cfff57ed84ea86a56e6447124744c294974c1
Sathvik-Malgikar <Sathvik.malgikar@gmail.com>
2024-01-19 10:05:31 +0530

    multiple carousels

--- public/projects.js
+++ public/projects.js
@@ -1,21 +1,48 @@
-// console.log("projects.js loaded")
+console.log("projects.js loaded")
 
-let currentIndex = 0;
+let carouselsCollection = [[0, [], 0], [0, [], 0], [0, [], 0]]
+
 const items = document.querySelectorAll('.carousel-item');
-const totalItems = items.length;
+// let itemsLength = items.length;
+for (let i = 0; i < items.length; i++) {
+    const element = items[i];
+    carouselsCollection[(Number(element.attributes.getNamedItem("data-index").value))][2] += 1
+    carouselsCollection[(Number(element.attributes.getNamedItem("data-index").value))][1].push(element)
 
-function showSlide(index) {
-    const carouselInner = document.getElementById('carousel-inner');
-    const newPosition = -index * 100 + '%';
-    carouselInner.style.transform = 'translateX(' + newPosition + ')';
+}
+// console.log(carouselsCollection[1][1])
+function showSlide(index, carouselInd) {
+    const carouselInners = document.getElementsByClassName('carousel-inner');
+
+    for (let i = 0; i < carouselInners.length; i++) {
+        const element = carouselInners.item(i)
+        if (Number(element.attributes.getNamedItem("data-index").value) != carouselInd) {
+            continue
+        }
+
+        const newPosition = -index * 100 + '%';
+        element.style.transform = 'translateX(' + newPosition + ')';
+
+    }
+
+
+
 }
 
-function nextSlide() {
-    currentIndex = (currentIndex + 1) % totalItems;
-    showSlide(currentIndex);
+function nextSlide(ind) {
+    ind = Number(ind)
+    console.log(ind)
+    console.log("next clicked")
+
+    carouselsCollection[ind][0] = (carouselsCollection[ind][0] + 1) % carouselsCollection[ind][2];
+    showSlide(carouselsCollection[ind][0], ind);
 }
 
-function prevSlide() {
-    currentIndex = (currentIndex - 1 + totalItems) % totalItems;
-    showSlide(currentIndex);
+function prevSlide(ind) {
+    ind = Number(ind)
+    console.log(ind)
+    console.log("previous clicked")
+
+    carouselsCollection[ind][0] = (carouselsCollection[ind][0] - 1) % carouselsCollection[ind][2];
+    showSlide(carouselsCollection[ind][0], ind);
 }
